530f4a3a482856ab95f834c1af6c70ef861ad80c
Sid-5137 <sidmallaofficial@gmail.com>
2025-11-06 17:01:15 +0530

    Modified frontent settings

--- frontend/frontend_app/settings.py
+++ frontend/frontend_app/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = "django-insecure-r-x!m!rjgbrs-3*$09=i2s$w(1#@c1=qjf&p8c4-6lvp8c*+p@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".onrender.com"]
 
 
 # Application definition
